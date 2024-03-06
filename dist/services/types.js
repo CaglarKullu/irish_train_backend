ddaa81ad6abd0bcff18856edce9bd0da44c68cfd
Caglar Kullu <97615706+CaglarKullu@users.noreply.github.com>
2024-03-06 17:35:26 +0000

    Delete dist directory

--- dist/services/types.js
+++
@@ -1,7 +0,0 @@
-export var StationType;
-(function (StationType) {
-    StationType["ALL"] = "A";
-    StationType["MAINLINE"] = "M";
-    StationType["SUBURBAN"] = "S";
-    StationType["DART"] = "D";
-})(StationType || (StationType = {}));
